58c3b53c374a7882717b4b782465e7804af94ee1
vasanth kumar r <vasanth3422@gmail.com>
2018-05-04 01:21:34 +0530

    email uniquness

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import path from 'path';
 import mongoose from "mongoose";
-import auth from './routes/auth';
 import bodyParser from "body-parser";
 import Promise from 'bluebird';
 import dotenv from 'dotenv';
 
+
+import auth from './routes/auth';
+import users from './routes/users';
 
 dotenv.config();
 const app = express();
@@ -14,6 +16,7 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL);
 
 app.use('/api/auth',auth);
+app.use('/api/users',users);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-
+import uniqueValidator from 'mongoose-unique-validator';
 
 const schema = new mongoose.Schema({
-  email:{type:String,required:true,lowercase:true,index:true},
-  passwordHash:{type:String,required:true}
+  email:{type:String,required:true,lowercase:true,index:true,unique:true},
+  passwordHash:{type:String,required:true},
+  confirmed:{type:Boolean, default:false}
 },
 {timestamp:true}
 );
@@ -19,12 +20,19 @@ schema.methods.generateJWT = function generateJWT(){
     email:this.email,
   },process.env.JWT_SECRET
 )};
+
+schema.methods.setPassword = function setPassword(password){
+  this.passwordHash = bcrypt.hashSync(password, 10)
+};
+
 schema.methods.toAuthJSON = function toAuthJSON(){
   return {
     email:this.email,
+    confirmed:this.confirmed,
     token:this.generateJWT()
   }
 };
 
+schema.plugin(uniqueValidator, {message:'This email is already taken'})
 
 export default mongoose.model('user',schema);
